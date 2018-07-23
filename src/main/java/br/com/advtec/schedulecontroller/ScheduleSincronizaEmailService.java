737917c3c216101a60f58661e0a02e1b43c4a038
William Franklin <william.reges1986@gmail.com>
2018-07-23 16:04:39 -0300

    refatorando a classe para clean code

--- src/main/java/br/com/advtec/schedulecontroller/ScheduleSincronizaEmailService.java
+++ src/main/java/br/com/advtec/schedulecontroller/ScheduleSincronizaEmailService.java
@@ -5,19 +5,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import br.com.advtec.services.BuscarEmailsServico;
+import br.com.advtec.services.BuscarAnexosDeEmailServico;
 
 @Service
 @EnableScheduling
 public class ScheduleSincronizaEmailService {
 	
 	@Autowired
-	private BuscarEmailsServico servicoBuscaEmail;
+	private BuscarAnexosDeEmailServico servicoBuscaEmail;
 
 
 	@Scheduled(fixedDelay= 1 * 60 * 1000)
 	public void buscarCaixaEmail() {
-		servicoBuscaEmail.buscarNotasDeEmailsXml();
+		servicoBuscaEmail.buscarEmailsComNotasAnexasXml();
 	}
 
 }
